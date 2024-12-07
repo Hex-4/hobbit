6fa7ade039a5c86882b345e78b2b397615d9d1b9
Hex-4 <121138331+Hex-4@users.noreply.github.com>
2024-12-06 22:41:04 -0700

    refactor market

--- bot.py
+++ bot.py
@@ -4,19 +4,38 @@ from dotenv import load_dotenv
 from tinydb import TinyDB, Query
 from datetime import datetime, time, timedelta, timezone
 from discord.ext import tasks
+import json
 
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot(intents=discord.Intents.all())
 db = TinyDB('db.json')
 lookup = Query()
 
+with open("market.json", mode="r", encoding="utf-8") as read_file:
+     market_data = json.load(read_file)["items"]
+
 unconfirmed_scheduled_message = None
 scheduled_messages = []
+shop_options = []
+
+
 
 @bot.event
 async def on_ready():
+    global market_data
     print(f"{bot.user} is ready and online!")
+    print(market_data)
     send_scheduled_messages.start()
+    generate_shop_options(market_data)
+
+def generate_shop_options(data):
+    global shop_options
+    
+    for i in data:
+        print(f"Name: {i["name"]}\nDesc: {i["description"]}\nValue:{i["id"]}")
+        option = discord.SelectOption(label=i["name"], description=i["description"], value=i["id"])
+        shop_options.append(option)
+        print(f"{len(shop_options)}")
     
 
 @bot.slash_command(name="hello", description="Say hello to the bot")
@@ -50,53 +69,56 @@ async def merits(ctx, user: discord.Option(discord.SlashCommandOptionType.user))
 
 
 class Shop(discord.ui.View):
-    @discord.ui.select( # the decorator that lets you specify the properties of the select menu
-        placeholder = "select something!", # the placeholder text that will be displayed if nothing is selected
-        min_values = 1, # the minimum number of values that must be selected by the users
-        max_values = 1, # the maximum number of values that can be selected by the users
-        options = [ # the list of options from which users can choose, a required field
-            discord.SelectOption(
-                label="Sigma Cake",
-                description="Sourced locally from Ohio."
-            ),
-            discord.SelectOption(
-                label="Chocolate",
-                description="Pick this if you like chocolate!"
-            ),
-            discord.SelectOption(
-                label="Strawberry",
-                description="Pick this if you like strawberry!"
-            )
-        ]
-    )
-    async def select_callback(self, select, interaction):  
-        choice = select.values[0]
-        select.disabled = True # set the status of the select as disabled 
+    def __init__(self, options):
+        super().__init__()
+        self.options = options
+
+        # Dynamically add the select menu with the options
+        self.select_menu = discord.ui.Select(
+            placeholder="Choose something to buy!",
+            min_values=1,
+            max_values=1,
+            options=self.options,
+            custom_id="shop_select"  # Unique identifier for the select menu
+        )
+        self.select_menu.callback = self.select_callback
+        self.add_item(self.select_menu)
+    async def select_callback(self, interaction):  
+        choice = self.select_menu.values[0]
+        self.select_menu.disabled = True # set the status of the select as disabled 
+        user_merits = db.search(lookup.id == interaction.user.id)[0].get('merits')
+
+
 
         await interaction.response.edit_message(view=self) # edit the message to show the changes
-        await interaction.followup.send("Are you sure?", view=Confirmation())  # Send a new message
+        
+        if user_merits < market_data[choice]["price"]:
+            await interaction.followup.send(f"You don't have enough <:Merit:1312943394854670398> Merits to buy {market_data[choice]['name']}!")
+        else:
+            await interaction.followup.send(f"Are you sure? This will cost you <:Merit:1312943394854670398> {market_data[choice]["price"]}, leaving you with <:Merit:1312943394854670398> {user_merits - market_data[choice]["price"]}", view=Confirmation())  # Send a new message
     
 
 class Confirmation(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
     @discord.ui.button(label="Yeah!", style=discord.ButtonStyle.green, emoji="<:checkmark:1313714877772337233>")
     async def button_callback(self, button, interaction):
-        await interaction.response.send_message("All done!") # Send a message when the button is clicked
+        button.disabled = True
+        await interaction.response.edit_message(view=self)
+        await interaction.followup.send("All done!") # Send a message when the button is clicked
 
 @bot.command(name="market", description="Buy something with your Merits!")
 async def flavor(ctx: discord.ApplicationContext):
+    global market_data
+    global shop_options
+    print(len(shop_options))
     if db.search(lookup.id == ctx.author.id):
         embed = discord.Embed(
             title="🛒 The Market",
             description="buy yourself something nice, eh?",
             color=discord.Colour.orange(),
         )
-        embed.add_field(name="<:Merit:1312943394854670398> 500", value="**Sigma Cake**\nSourced locally from Ohio.", inline=True)
-        embed.add_field(name="<:Merit:1312943394854670398> 500", value="**Sigma Cake**\nSourced locally from Ohio.", inline=True)
-        embed.add_field(name="<:Merit:1312943394854670398> 500", value="**Sigma Cake**\nSourced locally from Ohio.", inline=True)
-        embed.add_field(name="<:Merit:1312943394854670398> 500", value="**Sigma Cake**\nSourced locally from Ohio.", inline=True)
-        embed.add_field(name="<:Merit:1312943394854670398> 500", value="**Sigma Cake**\nSourced locally from Ohio.", inline=True)
-        embed.add_field(name="<:Merit:1312943394854670398> 500", value="**Sigma Cake**\nSourced locally from Ohio.", inline=True)
-        await ctx.respond(view=Shop(), embed=embed)
+        for i in market_data:
+            embed.add_field(name=f"<:Merit:1312943394854670398> {i["price"]}", value=f"**{i["name"]}**\n{i["description"]}", inline=True)
+        await ctx.respond(view=Shop(shop_options), embed=embed)
     else:
         await ctx.respond(f"{ctx.author.mention} has not joined the Merit system! Run `/start` to join.")
 
